c84dffb377d8d67f9592fcbf4b7c3c3872622c18
EFloresSchacht <140231087+Crohnoz@users.noreply.github.com>
2026-07-26 15:52:53 -0400

    style: modernize application typography
    Use DM Sans globally and improve typographic hierarchy and numeric readability.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import "./enhancements.css";
+import "./typography.css";
 import "./thermalPrint.css";
 import "./thermalPrint.js";
 
