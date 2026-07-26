3b9fe86c2fcd46e542aecb05cf0f5761385f938d
EFloresSchacht <140231087+Crohnoz@users.noreply.github.com>
2026-07-26 04:05:48 -0400

    feat: activar impresión térmica de vouchers

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import "./enhancements.css";
+import "./thermalPrint.css";
+import "./thermalPrint.js";
 
 createRoot(document.getElementById("root")).render(<App />);
