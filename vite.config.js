ddd3bd4572fd7bc41a2b168f4bac5b0b3a339bef
EFloresSchacht <140231087+Crohnoz@users.noreply.github.com>
2026-07-27 23:41:59 -0400

    fix: transform entrypoint before Vite HTML analysis

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ export default defineConfig({
   plugins: [
     {
       name: "use-resilient-entrypoint",
-      transformIndexHtml(html) {
-        return html.replace("/src/main.jsx", "/src/entry.jsx");
+      transformIndexHtml: {
+        order: "pre",
+        handler(html) {
+          return html.replace("/src/main.jsx", "/src/entry.jsx");
+        },
       },
     },
   ],
